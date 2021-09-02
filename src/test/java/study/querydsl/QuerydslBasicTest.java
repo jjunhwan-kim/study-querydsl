9bc98fd1f267d9d992835a1d75a625cbebd68941
nefertirii <nefertiri972@gmail.com>
2021-09-03 00:33:31 +0900

    Add join on clause
    - 조인 on절
      - 조인 대상 필터링(외부 조인)
        - 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
        - on절을 활용해 조인 대상을 필터링 할 때, 외부조인이 아니라 내부조인(inner join)을 사용하면 where절에서 필터링 하는 것과 동일
        - 내부조인 이면 where절로 해결하고, 외부조인이 필요한 경우에만 사용
      - 연관관계 없는 엔티티 외부 조인
        - 회원의 이름과 팀의 이름이 같은 대상 외부 조인
        - 일반조인: leftJoin(member.team, team)
        - on조인: from(member).leftJoin(team).on(xxx)

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -264,4 +264,43 @@ public class QuerydslBasicTest {
                 .extracting("username")
                 .containsExactly("teamA", "teamB");
     }
+
+    /**
+     * 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
+     * JPQL: select m, t from Member m left join m.team t on t.name = 'teamA'
+     */
+    @Test
+    void joinOnFiltering() {
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(member.team, team).on(team.name.eq("teamA"))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    /**
+     * 연관관계 없는 엔티티 외부 조인
+     * 회원의 이름이 팀 이름과 같은 대상 외부 조인
+     */
+    @Test
+    void joinOnNoRelation() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                //.leftJoin(member.team, team) // 일반조인일 경우 on절에 member.team_id = team.team_id 조건이 생성됨
+                .leftJoin(team).on(member.username.eq(team.name)) // 연관관계 없는 조인일 경우
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
 }
