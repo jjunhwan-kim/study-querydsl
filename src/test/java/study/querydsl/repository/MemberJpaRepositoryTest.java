b6fbfa0848e4dca38c7965810280a0aa47d8c0fa
nefertirii <nefertiri972@gmail.com>
2021-09-03 22:46:12 +0900

    Add jpa repository with querydsl
    - 순수 JPA 리포지토리 추가
    - 순수 JPA 리포지토리에 Querydsl 구현 추가
    - JPAQueryFactory 를 스프링 빈으로 등록해서 주입받아 사용해도 됨
    - 동시성 문제는 걱정하지 않아도 됨. 스프링이 주입해주는 엔티티 매니저는 실제 동작 시점에 진짜 엔티티 매니저를 찾아주는 프록시용 가짜 엔티티 매니저임. 가짜 엔티티 매니저는 실제 사용 시점에 트랜잭션 단위로 실제 엔티티 매니저(영속성 컨텍스트)를 할당해줌

---
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,47 @@
+package study.querydsl.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+
+import javax.persistence.EntityManager;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    void basicTest() {
+        Member member = new Member("member1", 10);
+        memberJpaRepository.save(member);
+
+        Member foundMember = memberJpaRepository.findById(member.getId()).get();
+        assertThat(foundMember).isEqualTo(member);
+
+        List<Member> result1 = memberJpaRepository.findAll();
+        assertThat(result1).containsExactly(member);
+
+        List<Member> result2 = memberJpaRepository.findByUsername("member1");
+        assertThat(result2).containsExactly(member);
+
+        List<Member> result3 = memberJpaRepository.findAllQuerydsl();
+        assertThat(result3).containsExactly(member);
+
+        List<Member> result4 = memberJpaRepository.findByUsernameQuerydsl("member1");
+        assertThat(result4).containsExactly(member);
+    }
+}
